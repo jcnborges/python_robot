3fb239fd5cd8978687d1ce7a5dc5f72c056340da
Júlio César Borges da Silveira Nardelli <jcn.borges@gmail.com>
2023-01-18 10:31:38 -0300

    Rotinas preliminares do robo

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import time
 from core.robo_rasp_zero_w import Robo_Rasp_Zero_W
 
 robo = Robo_Rasp_Zero_W()
 
 robo.setar_potencia_motor_esquerdo(50)
 robo.setar_potencia_motor_direito(50)
+
+robo.mover_frente()
+time.sleep(5)
+
